59b913fe83e1b39af4b0295b6a35a60a7c1a05c5
Moustafa Agamey <eng.mustafaelagamey@gmail.com>
2021-10-01 23:23:59 +0200

    Feat-77: Fetch record from database and display it in detail view

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import listing
+from .views import listing, detail
 
 app_name = 'books'
 urlpatterns = [
-    path('listing', listing, name='listing')
+    path('listing', listing, name='listing'),
+    path('detail/<id>', detail, name='detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -10,3 +10,14 @@ def listing(request):
         'books' :books
     }
     return render(request, 'books/listing.html',context=context)
+
+
+def detail(request, id):
+    try:
+        book = Book.objects.get(id=id)
+    except Book.DoesNotExist:
+        book = None
+    context = {
+        'book': book
+    }
+    return render(request, 'books/detail.html', context=context)
