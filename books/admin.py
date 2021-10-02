e0c8b7a60747b74f856f2ed295ddc6aa0fbf1761
Moustafa Agamey <eng.mustafaelagamey@gmail.com>
2021-10-02 20:12:34 +0200

    Feat-94: Edit related model to display data in the manymodel

--- books/admin.py
+++ books/admin.py
@@ -4,12 +4,16 @@ from django.contrib import admin
 
 # Register your models here.
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "first_name", "last_name")
+
+
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("rating",)
-    list_display = ("title","rating")
+    list_display = ("title", "rating", "author")
 
 
 admin.site.register(Book, BookAdmin)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
--- books/models.py
+++ books/models.py
@@ -9,6 +9,12 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.full_name()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Book(models.Model):
     title = models.CharField(max_length=80)
