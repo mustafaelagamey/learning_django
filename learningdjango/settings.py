6b769cf7cbc3296a6ee6d96650f42979f568c653
Moustafa Agamey <eng.mustafaelagamey@gmail.com>
2021-10-09 12:36:57 +0200

    Feat-116: Adding reviews form and template

--- learningdjango/settings.py
+++ learningdjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     # apps
     'challenges',
     'books',
+    'reviews',
     # default apps
     'django.contrib.admin',
     'django.contrib.auth',
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import review
+
 app_name = 'books'
 
 urlpatterns = [
+    path('', review)
 ]
